ff7b5ae94ff388f56d77f0b2e3148d4b8b2d16b0
nasbf <mo0ma@hotmail.com>
2025-11-14 09:53:18 -0500

    handle error and 500

--- routes/static.js
+++ routes/static.js
@@ -8,7 +8,9 @@ router.use(express.static(path.join(__dirname, "../public")));
 router.use("/css", express.static(path.join(__dirname + "../public/css")));
 router.use("/js", express.static(path.join(__dirname + "../public/js")));
 router.use("/images", express.static(path.join(__dirname + "../public/images")));
-
+router.get("/error", (req, res, next) => {
+  next(new Error("500 error for testing."));
+})
 module.exports = router;
 
 
--- server.js
+++ server.js
@@ -48,10 +48,10 @@ app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
   
-  if(err.status == 404){ message = err.message} 
-  else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  if(err.status == 404){ message = 'Oh no! There was a crash. Maybe try a different route?'} 
+  else {message = err.message}
   res.render("errors/error", {
-    title: err.status || 'Server Error',
+    title: err.status || 500,
     message,
     nav
   })
