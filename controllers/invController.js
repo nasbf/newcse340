ed25af46c8652bd57c679781226861a17e8c81e4
nasbf <mo0ma@hotmail.com>
2025-11-27 20:21:44 -0500

    Select Inventory Items

--- controllers/invController.js
+++ controllers/invController.js
@@ -15,7 +15,8 @@ invCont.buildByClassificationId = async function (req, res, next) {
   res.render("./inventory/classification", {
     title: className + " vehicles",
     nav,
-    grid
+    grid,
+    
   })
 }
 
@@ -43,9 +44,11 @@ invCont.buildByInvId = async function (req, res, next) {
  * ************************** */
 invCont.buildManagement = async function (req, res, next) {
   let nav = await utilities.getNav()
+  const classificationSelect = await utilities.buildClassificationList()
   res.render("inventory/management", {
     title: "Vehicle Management",
     nav,
+    classificationSelect,
     errors: null
   })
 }
@@ -167,5 +170,20 @@ invCont.addClassification = async function (req, res, next) {
     })
   }
 }
+
+/* ***************************
+ *  Return Inventory by Classification As JSON
+ * ************************** */
+invCont.getInventoryJSON = async (req, res, next) => {
+  const classification_id = parseInt(req.params.classification_id)
+  const invData = await invModel.getInventoryByClassificationId(classification_id)
+  if (invData[0].inv_id) {
+    return res.json(invData)
+  } else {
+    next(new Error("No data returned"))
+  }
+}
+
+
 
 module.exports = invCont
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
 const invValidate = require("../utilities/inventory-validation")
+const utilities = require("../utilities/")
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
@@ -11,6 +12,7 @@ router.get("/management", invController.buildManagement);
 router.get("/add-classification", invController.buildAddClassification);
 router.get("/add-vehicle", invController.buildAddNewVehicle);
 router.get("/add-classification", invController.buildAddNewVehicle);
+
 
 router.post(
   "/add-vehicle",
@@ -27,5 +29,8 @@ router.post(
   invValidate.checkClassificationData,
   invController.addClassification,
 )
+router.get(
+  "/getInventory/:classification_id", 
+  utilities.handleErrors(invController.getInventoryJSON))
 
 module.exports = router;
